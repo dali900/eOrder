fe3b92f509368dcf38f0e6d3bdc236a4b5bc2c9c
Dalibor <dalibor.gc@gmail.com>
2017-11-23 01:19:32 +0100

    Db config for OpenShift cloud

--- core/config.php
+++ core/config.php
@@ -7,12 +7,20 @@ return [
 		"password" => "gooddev1122",
 		"dbname" => "epiz_21050499_demo"
 	]*/
+	# OpenShift Cloud
 	"db"=> [
 		"servername" => "localhost",
 		"username" => "root",
 		"password" => "",
 		"dbname" => "eorder"
 	],
+	/*# Localhost
+	"db"=> [
+		"servername" => "mysql://mysql:3306/",
+		"username" => "eorder",
+		"password" => "eorder",
+		"dbname" => "eorder"
+	],*/
 	"error_reporting" => -1
 ]
  ?>
